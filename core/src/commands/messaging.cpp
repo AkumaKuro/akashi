0022e67ac2182be69b61be0b1abcd5a22aafecab
Salanto <62221668+Salanto@users.noreply.github.com>
2022-05-07 12:27:31 +0200

    Merge upstream (#15)
    * Don't Display IPIDs to Users via Global Chat
    
    Slightly better variable names too while we're at it
    
    * Ensure /changepass always has arg 1 as the password
    
    Otherwise, the syntax is /changepass <Password> or /changepass <Moderator> <Password>, rather than /changepass <Password> 'Moderator', like how it's specified to be
    
    * All hail the mightly clang-format check
    
    Co-authored-by: AwesomeAim <30537683+AwesomeAim@users.noreply.github.com>
    Co-authored-by: Rosemary Witchaven <32779090+in1tiate@users.noreply.github.com>

--- core/src/commands/messaging.cpp
+++ core/src/commands/messaging.cpp
@@ -76,10 +76,10 @@ void AOClient::cmdG(int argc, QStringList argv)
     QString l_sender_name = m_ooc_name;
     QString l_sender_area = server->getAreaName(m_current_area);
     QString l_sender_message = argv.join(" ");
-    // Slightly better readability
-    AOPacket l_packet = AOPacket("CT", {"[G][" + m_ipid + "][" + l_sender_area + "]" + l_sender_name, l_sender_message});
-    AOPacket l_other_packet = AOPacket("CT", {"[G][" + l_sender_area + "]" + l_sender_name, l_sender_message});
-    server->broadcast(l_packet, l_other_packet, Server::TARGET_TYPE::AUTHENTICATED);
+    // Better readability thanks to AwesomeAim.
+    AOPacket l_mod_packet = AOPacket("CT", {"[G][" + m_ipid + "][" + l_sender_area + "]" + l_sender_name, l_sender_message});
+    AOPacket l_user_packet = AOPacket("CT", {"[G][" + l_sender_area + "]" + l_sender_name, l_sender_message});
+    server->broadcast(l_user_packet, l_mod_packet, Server::TARGET_TYPE::AUTHENTICATED);
     return;
 }
 
